1e8cfe4d9cb20e22724f76fbf151be5f1a930f6c
Jorge Artieda <jorge.artieda@erni-espana.es>
2017-07-08 14:52:09 +0200

    exercise 3

---
+++ src/exercises/3/exercise.3.spec.ts
@@ -0,0 +1,30 @@
+// import { filterTheArray } from "./exercise.3";
+import { filterTheArray } from "./exercise.3";
+
+interface IFilterTheArray {
+    <T>(array: T[], filter: (val: T) => boolean): T[];
+}
+
+describe('FilterTheArray', () => {
+    let targetTest: IFilterTheArray = <any>filterTheArray;
+    it('should get a new array with pair values', () => {
+        expect(targetTest([1, 2, 4, 8], (value) => {
+            // only pairs
+            return value % 2 === 0;
+        })).toEqual([2, 4, 8]);
+    });
+
+    it('should get a new array with greater than 5 values', () => {
+        expect(targetTest([1, 2, 4, 8], (value) => {
+            // only greater than 5
+            return value > 5;
+        })).toEqual([8]);
+    });
+
+    it('should get a new array  ["Apple", "Pear"] ', () => {
+        expect(targetTest(["Car", "Tea", "Apple", "TypeScript", "Pear"], (value) => {
+            // only greater than 5
+            return value === "Apple" || value === "Pear";
+        })).toEqual(["Apple", "Pear"]);
+    });
+});
---
+++ src/exercises/3/exercise.3.ts
@@ -0,0 +1,14 @@
+
+/**
+ * Do you know how filter function works in arrays? let's build our own.
+ * Build a function that takes 2 parameters, an array (A) and a function (B), and will return another array (C). 
+ * The function parameter (B) will work as a filter, you can pass 1 parameter and it will return a boolean. 
+ * Depending on the value returned by B the original element of the array (A) should be / or not in the output array (C).
+ * Ex: A = [1, 2, 5, 4, 8], B -> onlyPair, -> C = [2, 4, 8]
+ * 
+ * The input array will never be null.
+ * The input array should not be modified!
+ */
+export function filterTheArray() {
+
+}
